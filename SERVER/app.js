82c5fa873fbdaa1585abeca1dcc5a1662c8dcae2
kevinever <nyayihakevin@gmail.com>
2018-11-18 11:27:15 +0200

    updating the functionality of the routes :envelope:

--- SERVER/app.js
+++ SERVER/app.js
@@ -6,13 +6,23 @@ const express = require('express');
 const app = express();
 
 
+
+const parcelsRoutes = require('./api/v1/routes/parcels');
+//const orderRoutes = require('./api/routes/order');
+
+app.use('/parcels', parcelsRoutes);
+//app.use('/order', orderRoutes);
+
+
+
+
 //interacting with the so-called middleware we use .use and pass some arguments to accep request, responds, and next to determine when the middleware is done its job and pass to the next middleware
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
    res.status(200).json({
      message: 'it works!'
 
    });
   
-});
+});*/
 
 module.exports = app;
