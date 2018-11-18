bec83395f118a641c0a1b996608d7f5640a597cf
kevinever <nyayihakevin@gmail.com>
2018-11-18 11:32:35 +0200

    [ft] :books: creating the CRUD of our parcel

---
+++ SERVER/api/v1/routes/parcels.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET requests to /parcels'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'Handling POST requests to /parcels'
+    });
+});
+
+router.get('/:parcelsId', (req, res, next) => {
+    const id = req.params.parcelsId;
+    if (id === '10') {
+        res.status(200).json({
+            message: 'Your parcel has been retrieved successfully',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'You passed an ID'
+        });
+    }
+});
+
+router.patch('/:parcelsId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Updated parcel!'
+    });
+});
+
+router.delete('/:parcelsId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Deleted parcel!'
+    });
+});
+
+module.exports = router;
