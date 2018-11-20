2cd46f7339724b7d0f4fb3124e54271de871acb1
kevinever <nyayihakevin@gmail.com>
2018-11-20 04:48:59 +0200

    ft-create-parcel:department_store:

---
+++ SERVER/api/v1/datastore/parcel.js
@@ -0,0 +1,22 @@
+
+
+const parcel=[];
+parcel.push({
+ 
+    id: '10',
+    origin:'kigali',
+    destination:'kampala',
+    userId:'10',
+    price:'10000'
+
+});
+
+
+parcel.push({
+
+   id: '11',
+    origin:'kigali',
+    destination:'kampala',
+    userId:'11',
+    price:'10000'
+});
