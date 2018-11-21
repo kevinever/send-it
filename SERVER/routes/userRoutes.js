1ead7fc56841ca1e66301ee10ce605edb8d04b46
kevinever <nyayihakevin@gmail.com>
2018-11-21 21:19:37 +0200

    [ft] updating-users-routes :bridge_at_night:

--- SERVER/routes/userRoutes.js
+++ SERVER/routes/userRoutes.js
@@ -1,13 +1,25 @@
 import express from 'express';
 import usersControllers from '../controllers/usersControllers';
+import parcelControllers from '../controllers/parcelControllers';
 
 const router=express.Router();
 
-router.get('/:userId/users',parcelControllers.getParcelsOfUser);
-router.post('/',usersControllers.create);
-router.get('/',usersControllers.getParcels);
-router.get('/:id',usersControllers.getParcelById);
-router.put('/:id/cancel',usersControllers.cancelParcel);
+router.get('/:userId/usersId', parcelControllers.getParcelsOfUser);
+
+// creating  new user
+router.post('/',  usersControllers.create);
 
 
+//fetching all the parcels from our datastore
+router.get('/',  usersControllers.getuserss);
+
+//fetching only one parcel by id 
+
+router.get('/:id',    usersControllers.getuserById);
+
+
+//apdating only by canceling  our created user
+router.put('/:id/cancel',  usersControllers.canceluser);
+
+//exportation of our router to enable them to use 
 export default router;
