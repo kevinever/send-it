c6ebedb91b326fee567602f7addf0d100c0a0f0d
kevinever <nyayihakevin@gmail.com>
2018-11-21 12:09:52 +0200

    [feature]-#161649368-adding our API data :books:

---
+++ SERVER/data/parcelData.js
@@ -0,0 +1,29 @@
+const parcels=[
+    {
+        id:"1",
+        userId:"11",
+        origin:"Kigali",
+        destination:"Ruhengeri",
+        weight:"20",
+        createdDate:"1/1/2018",
+        status:"pending",
+    },{
+        id:"2",
+        userId:"12",
+        origin:"Muhanga",
+        destination:"Rwamagana",
+        weight:"24",
+        createdDate:"1/09/2018",
+        status:"pending",
+    },
+    {
+        id:"3",
+        userId:"13",
+        origin:"Gisenyi",
+        destination:"Nyamata",
+        weight:"30",
+        createdDate:"1/07/2018",
+        status:"pending",
+    }
+]
+export default parcels
