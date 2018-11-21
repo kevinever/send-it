9498476423ee4ee4ac63fe69b970a86be00806ee
kevinever <nyayihakevin@gmail.com>
2018-11-21 11:58:41 +0200

    [ft]-git submodule add <url> UI/sendit-master

---
+++ SERVER/controllers/parcelControllers.js
@@ -0,0 +1,34 @@
+import parcels from '../data/parcelData';
+
+const parcelController={
+    create(req,res){
+        const{ origin,destination,weight }=req.body;
+        const newParcel={
+            id:(parcels.length+1).toString(),
+            userId:"23",
+            origin,
+            destination,
+            weight,
+            createdDate:new Date(),
+            status:"pending"
+        }
+    parcels.push(newParcel);
+    return res.status(201).send(newParcel);
+    },
+    getParcels(req,res){
+        return res.status(200).send(parcels);
+    },
+    getParcelById(req,res){
+        const id=req.params.id;
+        const oneParcel=parcels.find(parcel => parcel.id===id);
+        return res.status(200).send(oneParcel);
+    },
+    cancelParcel(req,res){
+        const id=req.params.id;
+        const oneParcel=parcels.find(parcel => parcel.id===id);
+        const index=parcels.indexOf(oneParcel);
+        parcels[index].status="canceled";
+        return res.status(201).send(parcels[index]);
+    }
+}
+export default parcelController;
