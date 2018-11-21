8eaf7ca0700ce20d843ab76ecf4085deed939eb5
kevinever <nyayihakevin@gmail.com>
2018-11-21 16:48:12 +0200

    [feature] routes-to-users :bridge_at_night:

--- SERVER/routes/parcelRoutes.js
+++ SERVER/routes/parcelRoutes.js
@@ -3,9 +3,9 @@ import parcelControllers from '../controllers/parcelControllers'
 
 const router = express.Router();
 // create a new parcel
-router.post('/',parcelControllers.create);
-router.get('/',parcelControllers.getParcels);
-router.get('/:id',parcelControllers.getParcelById);
-router.put('/:id/cancel',parcelControllers.cancelParcel);
+router.post('/',  parcelControllers.create);
+router.get('/',  parcelControllers.getParcels);
+router.get('/:id',  parcelControllers.getParcelById);
+router.put('/:id/cancel',  parcelControllers.cancelParcel);
 
 export default router;
