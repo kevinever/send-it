2f8775e61ad2c294d2695dc4a8d0084efdb24269
kevinever <nyayihakevin@gmail.com>
2018-11-27 12:50:46 +0200

    ft-parcel-db-control #162234639 :books:

---
+++ data/parcelData.js
@@ -0,0 +1,42 @@
+//creating our dummie data in objects within an array
+//id:"1",userId:"11",origin:"Kigali",destination:"Ruhengeri",weight:"20",createdDate:"1/1/2018",status:"pending",
+const parcels=[
+    {
+        id:"1",
+        userId:"11",
+        origin:"Kigali",
+        destination:"Ruhengeri",
+        weight:"20",
+        createdDate:"1/1/2018",
+        status:"pending",
+    },
+    
+    
+    
+    {
+        id:"2",
+        userId:"12",
+        origin:"Muhanga",
+        destination:"Rwamagana",
+        weight:"24",
+        createdDate:"1/09/2018",
+        status:"pending",
+    },
+
+
+
+
+    {
+        id:"3",
+        userId:"13",
+        origin:"Gisenyi",
+        destination:"Nyamata",
+        weight:"30",
+        createdDate:"1/07/2018",
+        status:"pending",
+    }
+]
+
+
+
+export default parcels
