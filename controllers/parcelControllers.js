9a337254ba245e961074515ba3c2fc4b5914abc8
kevinever <nyayihakevin@gmail.com>
2018-11-28 15:30:09 +0200

    ft-update-parcel-controllers :computer:

--- controllers/parcelControllers.js
+++ controllers/parcelControllers.js
@@ -1,6 +1,6 @@
 //we are gong to create our controllers to of our app to run on
 //importing parcels from our data dummies arrays
-import parcels from '../data/parcelData';
+//import parcels from '../data/parcelData';
 import execute from '../db/database';
 //id will be generated automatically as identifier of our parcels
 import uuid from 'uuid';
@@ -8,55 +8,81 @@ import uuid from 'uuid';
 //declaration of our controller that will help to CRUD as we want our created parcels
 
 //creating a parcelcontroler object  that will be used in CRUD
-const parcelController={
-    create(req, res){
-        const{ origin, destination, weight }=req.body;
-        const newParcel={
-            id:(parcels.length+1).toString(),
-            userId:uuid.v4(),
-            origin,
-            destination,
-            weight,
-            createdDate:new Date(),
-            status:"pending",
+const parcelController = {
+    async create(req, res){
+        const{ origin, destination,price,presentlocation, weight }=req.body;
+        let validInput =/^[\w ]+$/;
+            
+        if (!validInput.test(origin)){
+            //let newinput = firstname;
+            res.send("invalid origin");
+            return ;
+        }else if (!validInput.test(destination)){
+            //let newinput = firstname;
+            res.send("invalid destination");
+            return ;
         }
 
-        //insert as many parcels we want and return a message with new resource  in our temporary database
-        execute(`INSERT INTO parcels
-        VALUES($1, $2, $3, $4, $5, $6, $7, $8)`,[uuid.v4(),origin,destination,uuid.v4(),new Date(),400,'Kamonyi',weight])
-    return res.status(201).send(newParcel);
+        else if ( !validInput.test(price)){
+            //let newemail = email;
+            res.send("inavalid price");
+        }else {
+
+        // insert as many parcels we want and return a message with new resource  in our temporary database
+       const { rows } = await execute(`INSERT INTO parcels
+        VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`,[uuid.v4(),origin,destination,uuid.v4(),new Date(),price,presentlocation,weight,'pending'])
+        return res.status(201).send(rows[0]);
+        }
     },
 
    //fetch all parcels from our dummie databases
-    getParcels(req, res){
-        return res.status(200).send(parcels);
+   async getParcels(req, res){
+        const {rows}= await execute(`SELECT * FROM parcels`);
+        return res.status(200).send(rows);
     },
 
     //fetch parcel by id condition
-    getParcelById(req, res){
-        const id=req.params.id;
-        const oneParcel=parcels.find(parcel => parcel.id===id);
-        return res.status(200).send(oneParcel);
+    async getParcelById(req, res){
+        const id = req.params.id;
+        const{rows}=await execute(`SELECT * FROM parcels WHERE id = $1`,[id]) 
+        return res.status(200).send(rows[0]);
     },
 
 
      //we are going to then to allow to cancel our created parcel 
      //and again return changed status
 
-    cancelParcel(req, res){
+   async cancelParcel(req, res){
         const id=req.params.id;
-        const oneParcel=parcels.find(parcel => parcel.id===id);
-        const index=parcels.indexOf(oneParcel);
-        parcels[index].status="canceled";
-        return res.status(201).send(parcels[index]);
+        const {rows}= await execute(`UPDATE parcels SET status = $1 WHERE id = $2 RETURNING *`,['canceled',id])
+        return res.status(201).send(rows);
     },
-   
+       
+    
+    async changeDestination(req, res){
+        const id=req.params.id;
+        let destination = req.body.destination;
+        const {rows}= await execute(`UPDATE parcels SET destination = $1 WHERE id = $2 RETURNING *`,[destination,id])
+        return res.status(201).send(rows);
+    },
+
+
+    async changeLocation(req, res){
+        const id=req.params.id;
+        let location = req.body.presentLocation;
+        const {rows}= await execute(`UPDATE parcels SET presentLocation = $1 WHERE id = $2 RETURNING *`,[location,id])
+        return res.status(201).send(rows);
+    },
+
+
+
+
     //now we need to fetch parcels by userId
 
-    getParcelsOfUser(req, res){
+  async getParcelsOfUser(req, res){
      const userId=req.params.userId
-     const allParcels=parcels.filter(parcel=>parcel.userId===userId);
-     return res.status(200).send(allParcels);
+     const {rows}= await execute(`SELECT * FROM parcels WHERE user_id = $1`,[userId]);
+     return res.status(200).send(rows);
     }
 }
 
