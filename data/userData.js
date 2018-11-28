61fce3906c26ca5c3ae5b61516fe7c82d95ac0d9
kevinever <nyayihakevin@gmail.com>
2018-11-28 08:47:05 +0200

    ch-update-userdata-162234639  :book:

---
+++ data/userData.js
@@ -0,0 +1,17 @@
+const users=[
+    {
+        userId:"1",
+        usename:"kevin-ever",
+        email:"kevinever@lastking.alive",
+    },{
+        userId:"2",
+        usename:"nyayiha",
+        email:"nyayiha@lastking.alive",
+    },
+    {
+        userId:"3",
+        usename:"Rugamba-iteka",
+        email:"Rugamba-iteka@lastking.alive",
+    }
+]
+export default users
