eb1c05c80f34e8016be58a05671c9b02b51069dd
kevinever <nyayihakevin@gmail.com>
2018-11-29 16:41:30 +0200

    ft-update-UI-#161649202

---
+++ sendit/css/nav.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', function () {
+  mainNav.classList.toggle('active');
+});
